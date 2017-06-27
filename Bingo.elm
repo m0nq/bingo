43b34ffc834c9dadcbcac8fc9ef20e740887044a
Monk <forgetmenaught@gmail.com>
2017-06-26 23:00:25 -0400

    Understand that piping == composed functions

--- Bingo.elm
+++ Bingo.elm
@@ -3,5 +3,11 @@ module Bingo exposing (..)
 import Html
 
 
+-- main =
+--     Html.text (String.repeat 3 (String.toUpper "Mike's Game #3"))
+
+
 main =
-    Html.text "Hello, Bingo!"
+    "Mike's Game #3"
+        |> String.toUpper
+        |> Html.text
