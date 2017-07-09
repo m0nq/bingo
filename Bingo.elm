b26339f44e3cb10d59448afd7a5957c25b8435bc
Monk <forgetmenaught@gmail.com>
2017-07-08 21:13:22 -0400

    Add type annotations

--- Bingo.elm
+++ Bingo.elm
@@ -4,15 +4,18 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+playerInfo : String -> a -> String
 playerInfo name gameNumber =
     name ++ " - Game #" ++ (toString gameNumber)
 
 
+playerInfoText : String -> a -> Html msg
 playerInfoText name gameNumber =
     playerInfo name gameNumber
         |> String.toUpper
         |> text
 
 
+main : Html msg
 main =
     h2 [ id "info", class "classy" ] [ playerInfoText "Mike" 3 ]
